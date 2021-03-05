660dc57d61eafa2793d21957097db2f9f74a65be
Lynnibinny <72465633+Lynnibinny@users.noreply.github.com>
2021-03-05 22:17:07 +0100

    wir können mit flutter dart mit dem Button den php code aufrufen es gibt bellaa auf dem Webinterface aus. auch in die erste if schleife kommen wir rein aber die $Server und die $post profile variabeln sind leer.

--- lib/startgoals_widget.dart
+++ lib/startgoals_widget.dart
@@ -5,18 +5,33 @@ import 'dart:convert';
 //import 'dart:html';
 import 'package:http/http.dart' as http;
 
+class User {
+  final String promail;
+
+  User({this.promail});
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(promail: json['ProMail']);
+  }
+}
+
 class StartGoalsWidget extends StatelessWidget {
   //var promail = "uiuiuiuiui@me.com";
-  Future<http.Response> createUser(String promail) {
-    return http.post(
-      Uri.https("https://i-kf.ch/SaveATree/lib/insertdata.php", "ProMail"),
+  Future<User> createUser(String promail) async {
+    final response = await http.post(
+      Uri.https('i-kf.ch', '/SaveATree/lib/insertdata.php'),
       headers: <String, String>{
-        "Content-Type": "application/json; charset=UFT-8",
+        'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
-        "ProMail": promail,
+        'ProMail': promail,
       }),
     );
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      print('hurraa');
+    } else {
+      throw Exception('Faillll');
+    }
   }
 
   Widget build(BuildContext context) {
@@ -27,9 +42,9 @@ class StartGoalsWidget extends StatelessWidget {
         height: 250.0,
       ),
       RaisedButton(onPressed: () async {
-        var response = await createUser('test');
+        var response = await createUser('babybaum');
         print(response);
-        print(response.body);
+        // print(response.body);
       }),
       RaisedButton(
         onPressed: () {},
