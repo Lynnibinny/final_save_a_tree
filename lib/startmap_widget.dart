a542d3b4464b84ce49f0002440390391c2a64cbc
yarina141 <72451464+yarina141@users.noreply.github.com>
2021-04-07 11:53:43 +0200

    Map ibaut :)

--- lib/startmap_widget.dart
+++ lib/startmap_widget.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
-class StartMapWidget extends StatelessWidget {
+class StartMapWidget extends StatefulWidget {
+  @override
+  State<StartMapWidget> createState() => StartMapWidgetState();
+}
+
+class StartMapWidgetState extends State<StartMapWidget> {
+  Completer<GoogleMapController> _controller = Completer();
+  MapType type;
+  static final CameraPosition _cameraPosition = CameraPosition(
+    target: LatLng(-38, -70),
+    zoom: 4,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    type = MapType.hybrid;
+    markers = Set.from([]);
+  }
+
+  Set<Marker> markers;
+
+  @override
   Widget build(BuildContext context) {
-    const SizedBox(height: 30);
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Map'),
-        ),
-        body: Center(
-          child: RaisedButton(
-            onPressed: () {},
-            child: const Text('Enabled Button', style: TextStyle(fontSize: 20)),
+    return new Scaffold(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            markers: markers,
+            mapType: type,
+            onTap: (position) {
+              Marker mk1 = Marker(
+                markerId: MarkerId('1'),
+                position: position,
+              );
+              setState(() {
+                markers.add(mk1);
+              });
+            },
+            initialCameraPosition: _cameraPosition,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
           ),
-        ));
+          //SizedBox(child: Image.asset('assets/baum.jpg', fit: BoxFit.contain)),
+        ],
+      ),
+    );
   }
 }
