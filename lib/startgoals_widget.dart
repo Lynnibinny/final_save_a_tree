180b7b50f703d55dbf251b27033afedf55602f0c
yarina141 <72451464+yarina141@users.noreply.github.com>
2021-04-15 17:02:00 +0200

    http aus pubspec.yaml raus

--- lib/startgoals_widget.dart
+++ lib/startgoals_widget.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:save_a_tree/nav.dart';
+//import 'package:save_a_tree/nav.dart';
 import 'dart:async';
 import 'dart:convert';
-import 'dart:html';
-import 'package:http/http.dart' as http;
+//import 'dart:html';
+//import 'package:http/http.dart' as http;  //wieder reinnehmen(!)
 
 class User {
   final String promail;
@@ -15,50 +15,61 @@ class User {
 }
 
 class StartGoalsWidget extends StatelessWidget {
-  //var promail = "uiuiuiuiui@me.com";
-  Future<User> createUser(String promail) async {
-    //future da es in der Zukunft einen User machen wird. Es blockt also den Code nicht.
-    final response = await http.post(
-      Uri.https('i-kf.ch', '/SaveATree/lib/insertdata.php'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'ProMail': promail,
-      }),
-    );
-
-    //print(response.statusCode);
-    if (response.statusCode != 200) {
-      throw Exception('hurraa');
-    } else {
-      throw Exception('Faillll');
-    }
-    //throw Exception('hö?');
-  }
-
   Widget build(BuildContext context) {
-    const SizedBox(height: 30);
     return Scaffold(
-      body: Column(children: <Widget>[
-        SizedBox(
-          height: 250.0,
-        ),
-        RaisedButton(onPressed: () async {
-          var response = await createUser('babybaum');
-          print(response);
-          //print(response.body);
-        }),
-        RaisedButton(
-          onPressed: () {},
-          child: const Text('Enabled Button', style: TextStyle(fontSize: 20)),
-        ),
-        RaisedButton(onPressed: () async {
-          var response = await createUser('test');
-          print(response);
-          //print(response.body);
-        }),
-      ]),
+      appBar: AppBar(
+        title: Text('Spaceholderscreen for "StartGoalsWidget"'),
+      ),
+      body: Center(
+        child: RaisedButton(
+            onPressed: () {}, color: Colors.green, child: Text(':)')),
+      ),
     );
   }
+  // //var promail = "uiuiuiuiui@me.com";
+  // Future<User> createUser(String promail) async {
+  //   //future da es in der Zukunft einen User machen wird. Es blockt also den Code nicht.
+  //   final response = await http.post(
+  //     Uri.https('i-kf.ch', '/SaveATree/lib/insertdata.php'),
+  //     headers: <String, String>{
+  //       'Content-Type': 'application/json; charset=UTF-8',
+  //     },
+  //     body: jsonEncode(<String, String>{
+  //       'ProMail': promail,
+  //     }),
+  //   );
+
+  //   //print(response.statusCode);
+  //   if (response.statusCode != 200) {
+  //     print('hurraa');
+  //   } else {
+  //     throw Exception('Faillll');
+  //   }
+  //   //throw Exception('hö?');
+  // }
+
+  // Widget build(BuildContext context) {
+  //   const SizedBox(height: 30);
+  //   return Scaffold(
+  //     body: Column(children: <Widget>[
+  //       SizedBox(
+  //         height: 250.0,
+  //       ),
+  //       RaisedButton(onPressed: () async {
+  //         var response = await createUser('babybaum');
+  //         print(response);
+  //         //print(response.body);
+  //       }),
+  //       RaisedButton(
+  //         onPressed: () {},
+  //         child: const Text('Enabled Button', style: TextStyle(fontSize: 20)),
+  //       ),
+  //       RaisedButton(onPressed: () async {
+  //         var response = await createUser('test');
+  //         print(response);
+  //         //print(response.body);
+  //       }),
+  //     ]),
+  //   );
+  // }
 }
--- lib/startprofile_widget.dart
+++ lib/startprofile_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:save_a_tree/startmap_widget.dart';
+//import 'package:save_a_tree/startmap_widget.dart';
 import 'package:save_a_tree/profileSettings_widget.dart';
 
 class StartProfileWidget extends StatelessWidget {
