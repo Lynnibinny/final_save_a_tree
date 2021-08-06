2ff910b5d1a8bbaac86d9a147e71205765550262
Lynnibinny <72465633+Lynnibinny@users.noreply.github.com>
2021-08-06 20:02:15 +0200

    registrieren und login button grün

--- lib/firstscreen.dart
+++ lib/firstscreen.dart
@@ -159,8 +159,10 @@ class _FirstScreenState extends State<FirstScreen> {
     final loginButon = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
+      color: Colors.lightGreen[700],
+    
       child: MaterialButton(
+        
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         onPressed: () {
@@ -175,7 +177,7 @@ class _FirstScreenState extends State<FirstScreen> {
     final registrationButon = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
+      color: Colors.lightGreen[700],
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
