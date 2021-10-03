b802f19f3f9ae6de3f2c09d0bdca6e1e759db9f6
yarina141 <72451464+yarina141@users.noreply.github.com>
2021-10-03 18:04:52 +0200

    Tajy Poty added

---
+++ lib/Map/coordinates.dart
@@ -0,0 +1,78 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+final List<LatLng> fPuertoLeoni = [
+  //Polygon's edges
+  LatLng(-26.9701194, -55.191528),
+  LatLng(-26.974547, -55.190064),
+  LatLng(-26.973308, -55.185219),
+  LatLng(-26.968797, -55.186753),
+];
+
+final List<LatLng> fProvincialPark = [
+  //Polygon's edges
+  LatLng(-26.9417833, -54.9236667),
+  LatLng(-26.9417847, -54.9095361),
+  LatLng(-26.9612139, -54.9223056),
+  LatLng(-26.9559028, -54.9362750),
+];
+
+final List<LatLng> fColoniaDelicia = [
+  //Polygon's edges
+  LatLng(-27.23813056, -54.38782778),
+  LatLng(-27.23260556, -54.35243889),
+  LatLng(-27.23967222, -54.35005),
+  LatLng(-27.24286667, -54.357375),
+  LatLng(-27.24620556, -54.35780833),
+  LatLng(-27.25083889, -54.34773611),
+  LatLng(-27.25454444, -54.34881667),
+  LatLng(-27.24928333, -54.36295278),
+  LatLng(-27.24089722, -54.362725),
+  LatLng(-27.24531389, -54.38560833),
+];
+
+final List<LatLng> fTupambaE = [
+  //Polygon's edges
+  LatLng(-26.99685, -55.057425),
+  LatLng(-26.99925833, -55.05991944),
+  LatLng(-27.00076389, -55.05899722),
+  LatLng(-26.99992778, -55.0604638),
+  LatLng(-27.00327222, -55.06044167),
+  LatLng(-27.008075, -55.05334167),
+  LatLng(-27.00616389, -55.05259722),
+  LatLng(-27.00416667, -55.05451944),
+  LatLng(-27.00243611, -55.05400278),
+  LatLng(-27.00140833, -55.05663611),
+  LatLng(-27.00081944, -55.05653611),
+  LatLng(-27.00132222, -55.054225),
+  LatLng(-27.00073611, -55.05390833),
+  LatLng(-27.00128611, -55.05217778),
+  LatLng(-26.99974722, -55.05150833),
+  LatLng(-26.99835556, -55.04930556),
+];
+
+final List<LatLng> fChafariz = [
+  //Polygon's edges
+  LatLng(-26.9816000, -55.0939639),
+  LatLng(-26.9819528, -55.1143861),
+  LatLng(-27.0010444, -55.1150278),
+  LatLng(-27.0007028, -55.0973583),
+];
+
+final List<LatLng> fTajyPoty = [
+  //Polygon's edges
+  LatLng(-27.0623750, -55.0494333),
+  LatLng(-27.0623583, -55.0519472),
+  LatLng(-27.0646111, -55.0519333),
+  LatLng(-27.0645139, -55.0548667),
+  LatLng(-27.0665528, -55.0554611),
+  LatLng(-27.0712056, -55.0551194),
+  LatLng(-27.0711000, -55.0484111),
+  LatLng(-27.0679028, -55.0482222),
+];
+
+LatLng mPuertoLeoni = LatLng(-26.969627380371094, -55.189170837402344);
+LatLng mProvincialPark = LatLng(-26.945327758789062, -54.92672348022461);
+LatLng mColoniaDelicia = LatLng(-27.24620556, -54.35780833);
+LatLng mTupambaE = LatLng(-27.00416667, -55.05451944);
+LatLng mChafariz = LatLng(-26.9816000, -55.0939639);
+LatLng mTajyPoty = LatLng(-27.0623750, -55.0494333);
--- lib/Map/startmap_widget.dart
+++ lib/Map/startmap_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-//import 'package:save_a_tree/mapStyle.dart';
 import 'package:save_a_tree/Map/mapstyle.dart';
 import 'package:save_a_tree/Map/marker.dart';
+import 'package:save_a_tree/Map/coordinates.dart';
 
 class StartMapWidget extends StatefulWidget {
   @override
@@ -12,80 +12,6 @@ class StartMapWidget extends StatefulWidget {
 class StartMapWidgetState extends State<StartMapWidget> {
   Set<Marker> _markers = {};
   Set<Polygon> _polygons = {};
-
-  //Test
-  /*
-  final List<LatLng> posPolygon_1 = [
-    //Polygon's edges
-    LatLng(-50, -50),
-    LatLng(-30.196338, -50),
-    LatLng(-20, -100),
-    LatLng(-100, -40),
-  ];
-  */
-
-  final List<LatLng> fPuertoLeoni = [
-    //Polygon's edges
-    LatLng(-26.9701194, -55.191528),
-    LatLng(-26.974547, -55.190064),
-    LatLng(-26.973308, -55.185219),
-    LatLng(-26.968797, -55.186753),
-  ];
-
-  final List<LatLng> fProvincialPark = [
-    //Polygon's edges
-    LatLng(-26.9417833, -54.9236667),
-    LatLng(-26.9417847, -54.9095361),
-    LatLng(-26.9612139, -54.9223056),
-    LatLng(-26.9559028, -54.9362750),
-  ];
-
-  final List<LatLng> fColoniaDelicia = [
-    //Polygon's edges
-    LatLng(-27.23813056, -54.38782778),
-    LatLng(-27.23260556, -54.35243889),
-    LatLng(-27.23967222, -54.35005),
-    LatLng(-27.24286667, -54.357375),
-    LatLng(-27.24620556, -54.35780833),
-    LatLng(-27.25083889, -54.34773611),
-    LatLng(-27.25454444, -54.34881667),
-    LatLng(-27.24928333, -54.36295278),
-    LatLng(-27.24089722, -54.362725),
-    LatLng(-27.24531389, -54.38560833),
-  ];
-
-  final List<LatLng> fTupambaE = [
-    //Polygon's edges
-    LatLng(-26.99685, -55.057425),
-    LatLng(-26.99925833, -55.05991944),
-    LatLng(-27.00076389, -55.05899722),
-    LatLng(-26.99992778, -55.0604638),
-    LatLng(-27.00327222, -55.06044167),
-    LatLng(-27.008075, -55.05334167),
-    LatLng(-27.00616389, -55.05259722),
-    LatLng(-27.00416667, -55.05451944),
-    LatLng(-27.00243611, -55.05400278),
-    LatLng(-27.00140833, -55.05663611),
-    LatLng(-27.00081944, -55.05653611),
-    LatLng(-27.00132222, -55.054225),
-    LatLng(-27.00073611, -55.05390833),
-    LatLng(-27.00128611, -55.05217778),
-    LatLng(-26.99974722, -55.05150833),
-    LatLng(-26.99835556, -55.04930556),
-  ];
-
-  final List<LatLng> fChafariz = [
-    LatLng(-26.9816000, -55.0939639),
-    LatLng(-26.9819528, -55.1143861),
-    LatLng(-27.0010444, -55.1150278),
-    LatLng(-27.0007028, -55.0973583),
-  ];
-
-  LatLng mPuertoLeoni = LatLng(-26.969627380371094, -55.189170837402344);
-  LatLng mProvincialPark = LatLng(-26.945327758789062, -54.92672348022461);
-  LatLng mColoniaDelicia = LatLng(-27.24620556, -54.35780833);
-  LatLng mTupambaE = LatLng(-27.00416667, -55.05451944);
-  LatLng mChafariz = LatLng(-26.9816000, -55.0939639);
 
   BitmapDescriptor customGreenMarker;
   BitmapDescriptor customRedMarker; //class for our custom Marker
@@ -191,6 +117,7 @@ class StartMapWidgetState extends State<StartMapWidget> {
     addRedPolygon(fColoniaDelicia);
     addRedPolygon(fTupambaE);
     addGreenPolygon(fChafariz);
+    addRedPolygon(fTajyPoty);
 
     //Add markers to the list of markers
     addGreenMarker(mPuertoLeoni);
@@ -198,6 +125,7 @@ class StartMapWidgetState extends State<StartMapWidget> {
     addRedMarker(mColoniaDelicia);
     addRedMarker(mTupambaE);
     addGreenMarker(mChafariz);
+    addRedMarker(mTajyPoty);
   }
 
   @override
