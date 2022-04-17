6dbad9276c3082b81b0aaef3954870d5ef9f1418
Lynnibinny <72465633+Lynnibinny@users.noreply.github.com>
2022-04-17 11:24:23 +0200

    erste Pushnotification

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Lynn on 14.04.22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
